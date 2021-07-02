fb285d321dea9b76195f6324af3849ab88a4ae43
rikanga <richardkangamba3@gmail.com>
2021-07-02 17:54:45 +0100

    undestand the compute of variance with python

---
+++ variance.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+import numpy as np
+
+# create numpy array
+arr = np.array([30, 17, 15])
+
+# calculate a variance 
+variance = np.var(arr)
+
+# an other method for calculating variance
+var = var.var()
+
+"""
+Test
+
+print(var) --> 44.22222222222222
+"""
